8b840c81c66825035d7c6e7f373a6fac45e9d422
Yao Qiang <qiangyao1988@126.com>
2021-11-27 23:26:42 -0500

    add C++ and Java files

---
+++ 160. Intersection of Two Linked Lists/solution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+
+        if (p1 == null && p2 == null) return null;
+
+        while (p1 != null && p2 != null && p1 != p2) {
+            p1 = p1.next;
+            p2 = p2.next;
+
+            //
+            // Any time they collide or reach end together without colliding 
+            // then return any one of the pointers.
+            //
+            if (p1 == p2) return p1;
+
+            //
+            // If one of them reaches the end earlier then reuse it 
+            // by moving it to the beginning of other list.
+            // Once both of them go through reassigning, 
+            // they will be equidistant from the collision point.
+            //
+            if (p1 == null) p1 = headB;
+            if (p2 == null) p2 = headA;
+        }
+
+        return p1;
+    }
+}
